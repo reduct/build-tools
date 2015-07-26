c6443770ad0ed6e503bb07b5e6767eb57d374962
Tyll Weiß <weiss@sitegeist.de>
2015-07-26 16:06:25 +0200

    [TASK] Add a catch statement on each task call for error logging purposes

--- Bin/Reduct.js
+++ Bin/Reduct.js
@@ -8,31 +8,36 @@ var test = require('./Test.js');
 var userArgs = process.argv.slice(2);
 var taskType = userArgs[0];
 
+function onError(err) {
+    console.log(err)
+    process.exit(1);
+}
+
 switch(taskType) {
     case 'lint':
-        lint();
+        lint().catch(onError);
 
         break;
 
     case 'publish-coverage':
-        publishCoverage();
+        publishCoverage().catch(onError);
 
         break;
 
     case 'build':
-        build();
+        build().catch(onError);
 
         break;
 
     case 'mocha':
-        test();
+        test().catch(onError);
 
         break;
 
     case 'test':
         lint().then(function() {
-            test();
-        });
+            return test();
+        }).catch(onError);
 
         break;
 
@@ -41,7 +46,7 @@ switch(taskType) {
             return test();
         }).then(function() {
             return build();
-        });
+        }).catch(onError);
 
         break;
 }
