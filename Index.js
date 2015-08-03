56ad03e3959c7ba8a075d48f8c90018ae455a0e4
Tyll Weiß <inkdpixels@gmail.com>
2015-08-03 20:15:53 +0200

    [FEATURE] Add a helper utility for creating DOM mocks with jsdom

--- Index.js
+++ Index.js
@@ -2,5 +2,6 @@ module.exports = {
 	mocha: require('mocha'),
 	chai: require('chai'),
 	jsdom: require('jsdom'),
-	babel: require('babel')
+	babel: require('babel'),
+	mock: require('./Dist/Helpers/DOM.js')
 };
